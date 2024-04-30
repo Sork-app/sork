c611001938762646aea02d9ae2bdeb9e1a79779e
Pranav Ramesh <laphatize@protonmail.com>
2024-04-30 03:12:57 -0400

    chore: attempt to fix vercel build errors
    Signed-off-by: Pranav Ramesh <laphatize@protonmail.com>

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import mongoose from '../../utils/db';
 
 
 export default function handler(req, res) {
--- src/pages/models/Organization.js
+++ src/pages/models/Organization.js
@@ -1,4 +1,4 @@
-const mongoose = require('../utils/db.js'); // Adjust the path as necessary
+const mongoose = require('../../utils/db.js');
 const { Schema } = mongoose;
 
 const organizationSchema = new Schema({
